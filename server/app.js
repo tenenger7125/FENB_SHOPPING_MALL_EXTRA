c6d546e41c4a821bec1d425d58927d02ee882315
Lee Dong Gyu <tenenger7125@gmail.com>
2023-04-24 10:46:02 +0900

    feat: add cors

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 require('dotenv').config();
 
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 8000;
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use('/api', api);
 
