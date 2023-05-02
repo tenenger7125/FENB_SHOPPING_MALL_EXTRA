0694dff6fd57707f926ef75de0840b88d0f188dd
KimKyungWoo <60132428+KimKyungWoo94@users.noreply.github.com>
2023-05-02 20:14:30 +0900

    feat: add HTML TAb Logo (#54)
    * feat: Modify Products Images Position and background
    
    * feat: modify asics, converse, ecco shoes width, heigt
    
    * feat: add ScrollToTp and modify products image
    
    * feat: modify direction products image
    
    * feat: add responsive styles SignIn and SignUp
    
    * feat: Modify ScrollToTop Component Name, SignIn and SignUp resposiveStyle
    
    * feat: modify Component Name and add Shoes Image
    
    * feat: add Products image
    
    * add Product Imgae and modify style
    
    * feat: modify FormEmailInput Component Name, Remove import React
    
    * feat: add Product Image
    
    * Feat: Modify HTML Tab Logo
    
    * feat: modify userState Recoil

--- client/src/recoil/atoms/userState.js
+++ client/src/recoil/atoms/userState.js
@@ -1,20 +1,8 @@
 import { atom } from 'recoil';
 
 const KEY = 'user';
-// const userState = atom({
-//   key: 'userState',
-//   default: {},
-// });
 
-/**
- * Atom Effects
- * @see https://recoiljs.org/ko/docs/guides/atom-effects/
- */
 const localStorageEffect = ({ onSet }) => {
-  /**
-   * onSet은 상태가 변경되면 호출된다.
-   * 상태가 업데이트될 때마다 localStorage에 저장한다.
-   */
   onSet(newState => {
     localStorage.setItem(KEY, JSON.stringify(newState));
   });
