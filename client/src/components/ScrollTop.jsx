81def64f5ba399a7e21fc547b002cca625bb909d
LEE DONG GYU <tenenger7125@gmail.com>
2023-05-10 15:23:15 +0900

    style: change scroll to top color

--- client/src/components/ScrollTop.jsx
+++ client/src/components/ScrollTop.jsx
@@ -8,7 +8,7 @@ const ScrollTop = ({ positionY }) => {
   return (
     <>
       {scroll.y >= positionY && (
-        <ActionIcon color="indigo" size="3rem" pos="fixed" bottom="2rem" right="2rem" sx={{ borderRadius: '50%' }}>
+        <ActionIcon color="blue.6" size="3rem" pos="fixed" bottom="2rem" right="2rem" sx={{ borderRadius: '50%' }}>
           <SlArrowUpCircle onClick={() => scrollTo({ y: 0 })} size="5rem" />
         </ActionIcon>
       )}
