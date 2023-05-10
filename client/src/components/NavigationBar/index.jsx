7bf36c816792779f6a6eb198fa367a63371e2311
LEE DONG GYU <tenenger7125@gmail.com>
2023-05-10 15:34:32 +0900

    style: add navigation bar margin bottom and main page margin top

--- client/src/components/NavigationBar/index.jsx
+++ client/src/components/NavigationBar/index.jsx
@@ -3,7 +3,7 @@ import Category from './Category';
 import Main from './Main';
 
 const NavigationBar = () => (
-  <Navbar height="auto" zIndex={9999} position={{ top: 0, left: 0, borderBottom: '1px solid #ced4da' }}>
+  <Navbar height="auto" mb="4rem" position={{ top: 0, left: 0, borderBottom: '1px solid #ced4da' }}>
     <Container w="100%" size="120rem" m="auto">
       <Main />
       <Category />
--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useDisclosure } from '@mantine/hooks';
+import { Container } from '@mantine/core';
 import { Carousel, Modal, Products, Loading } from '../components/Main';
 
 const Main = () => {
@@ -7,12 +8,12 @@ const Main = () => {
   const [modalIsOpen, { open, close }] = useDisclosure(false);
 
   return (
-    <>
+    <Container fluid p="0" mt="-4rem">
       <Carousel modalOpen={open} setModalTitle={setTitle} />
       <Products />
       <Modal title={title} modalIsOpen={modalIsOpen} modalClose={close} />
       <Loading />
-    </>
+    </Container>
   );
 };
 export default Main;
