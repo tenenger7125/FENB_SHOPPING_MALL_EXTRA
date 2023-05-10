2999973bc0e2a02ba6508d775b895628c6f7dff6
aesumin <2607gk@naver.com>
2023-05-10 16:51:15 +0900

    refactor: refactoring order complete page css

--- client/src/components/OrderComplete/OrderProducts.jsx
+++ client/src/components/OrderComplete/OrderProducts.jsx
@@ -9,26 +9,26 @@ const OrderProducts = ({ products }) => {
       <Title fz="2.4rem" mb="2rem" sx={{ textAlign: 'center' }}>
         주문 상품
       </Title>
-      <Stack py="1.6rem" px="3.2rem" sx={{ border: '1px solid lightgray', borderRadius: '5px' }}>
-        {products.map(({ selectedSize, quantity, feature, name, price, color, imgURL, id }) => (
+      <Stack py="1.6rem" px="3.2rem" spacing="2rem" sx={{ border: '1px solid lightgray', borderRadius: '5px' }}>
+        {products.map(({ selectedSize, quantity, name, price, color, imgURL, id }) => (
           <Group key={id} position="center" align="flex-start" fz="1.4rem">
             <div style={{ width: '20%', minWidth: '110px' }}>
-              <Image src={imgURL} alt={name} withPlaceholder sx={{ img: { width: '110px' } }} />
+              {/* <Image src={imgURL} alt={name} withPlaceholder sx={{ img: { width: '100px' } }} /> */}
+              <Image src={imgURL} alt={name} withPlaceholder width="11rem" height="11rem" />
             </div>
             <Group position="apart" align="flex-start" justify="center" w="70%" my="auto">
-              <Stack spacing={0} maw="fit-content" justify="center">
-                <Title fz="1.6rem" fw="bold" c={colorScheme === 'dark' ? 'gray.6' : '#111'}>
+              <Stack spacing="0.2rem" maw="fit-content" justify="center">
+                <Title fz="1.6rem" fw="bold" c={colorScheme === 'dark' ? 'gray.4' : '#111'}>
                   {name}
                 </Title>
-                <Text pl="0.2rem">{feature}</Text>
-                <Text pl="0.2rem">사이즈 {selectedSize}</Text>
-                <Text pl="0.2rem">색상 {COLORS[color].kr}</Text>
+                <Text pl="0.2rem">사이즈 : {selectedSize}</Text>
+                <Text pl="0.2rem">색상 : {COLORS[color].kr}</Text>
                 <Text pl="0.2rem">
-                  {quantity} / {price.toLocaleString()}
+                  수량/상품가격 : {quantity} / {price.toLocaleString()} 원
                 </Text>
               </Stack>
               <Stack>
-                <Text fz="1.6rem" fw="bold" c={colorScheme === 'dark' ? 'gray.6' : '#111'}>
+                <Text fz="1.6rem" fw="bold" c={colorScheme === 'dark' ? 'gray.4' : '#111'}>
                   {(price * quantity).toLocaleString()} 원
                 </Text>
               </Stack>
