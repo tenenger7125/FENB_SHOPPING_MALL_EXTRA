eb4aaeac93c4f9da81dfa945bd50835d3410c7db
aesumin <2607gk@naver.com>
2023-05-11 14:04:47 +0900

    feat : add order complete page responsive style

--- client/src/components/OrderComplete/OrderInfo.jsx
+++ client/src/components/OrderComplete/OrderInfo.jsx
@@ -1,5 +1,8 @@
 import { Stack, Title, Group, Text } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import { PAYMENT_METHODS } from '../../constants';
+
+const MEDIAQUERY_WIDTH = 768;
 
 const OrderInfo = ({ history }) => {
   const { orderDate, paymentMethod, discountedTotalPrice, deliveryAddress } = history;
@@ -8,30 +11,40 @@ const OrderInfo = ({ history }) => {
   const orderedDate = new Date(orderDate);
   const payment = PAYMENT_METHODS.find(methods => methods.value === paymentMethod).label;
 
+  const matches = useMediaQuery(`(min-width: ${MEDIAQUERY_WIDTH}px)`);
+
   return (
-    <Stack w="70%" p="2rem">
+    <Stack w={matches ? '70%' : '90%'} p="2rem">
       <Title fz="2.4rem" mb="2rem" sx={{ textAlign: 'center' }}>
         주문 정보
       </Title>
-      <Group py="1.6rem" px="3.2rem" spacing="3.2rem" sx={{ border: '1px solid lightgray', borderRadius: '5px' }}>
-        <Stack>
+      <Group
+        py="1.6rem"
+        px={matches ? '3.2rem' : '1.6rem'}
+        spacing={matches ? '3.2rem' : '1.6rem'}
+        sx={{ border: '1px solid lightgray', borderRadius: '5px' }}>
+        <Stack miw="7rem" w="10%">
           <Text>받는 사람</Text>
           <Text>주소</Text>
           <Text>전화번호</Text>
         </Stack>
-        <Stack>
+        <Stack miw="18rem" w="70%">
           <Text>{deliveryAddress.recipient}</Text>
           <Text>{address}</Text>
           <Text>{deliveryAddress.recipientPhone}</Text>
         </Stack>
       </Group>
-      <Group py="1.6rem" px="3.2rem" spacing="3.2rem" sx={{ border: '1px solid lightgray', borderRadius: '5px' }}>
-        <Stack>
+      <Group
+        py="1.6rem"
+        px={matches ? '3.2rem' : '1.6rem'}
+        spacing={matches ? '3.2rem' : '1.6rem'}
+        sx={{ border: '1px solid lightgray', borderRadius: '5px' }}>
+        <Stack miw="7rem" w="10%">
           <Text>결제 금액</Text>
           <Text>결제 방식</Text>
           <Text>결제 시각</Text>
         </Stack>
-        <Stack>
+        <Stack miw="18rem" w="70%">
           <Text>{discountedTotalPrice.toLocaleString()} 원</Text>
           <Text>{payment}</Text>
           <Text>{orderedDate.toLocaleString('ko-KR')}</Text>
--- client/src/components/OrderComplete/OrderProducts.jsx
+++ client/src/components/OrderComplete/OrderProducts.jsx
@@ -1,19 +1,33 @@
 import { Stack, Title, Group, Image, Text, useMantineColorScheme } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import { COLORS } from '../../constants';
 
+const MEDIAQUERY_WIDTH = 768;
+
 const OrderProducts = ({ products }) => {
+  const matches = useMediaQuery(`(min-width: ${MEDIAQUERY_WIDTH}px)`);
   const { colorScheme } = useMantineColorScheme();
 
   return (
-    <Stack w="70%" p="2rem">
+    <Stack w={matches ? '70%' : '90%'} p="2rem">
       <Title fz="2.4rem" mb="2rem" sx={{ textAlign: 'center' }}>
         주문 상품
       </Title>
-      <Stack py="1.6rem" px="3.2rem" spacing="2rem" sx={{ border: '1px solid lightgray', borderRadius: '5px' }}>
+      <Stack
+        py="1.6rem"
+        px={matches ? '3.2rem' : '1.6rem'}
+        spacing="2rem"
+        sx={{ border: '1px solid lightgray', borderRadius: '5px' }}>
         {products.map(({ selectedSize, quantity, name, price, color, imgURL, id }) => (
-          <Group key={id} position="center" align="flex-start" fz="1.4rem">
-            <div style={{ width: '20%', minWidth: '110px' }}>
-              <Image src={imgURL} alt={name} withPlaceholder width="11rem" height="11rem" />
+          <Group key={id} justify="center" fz="1.4rem">
+            <div style={{ width: '20%', minWidth: `${matches ? '10.8rem' : '7.8rem'}` }}>
+              <Image
+                src={imgURL}
+                alt={name}
+                withPlaceholder
+                width={matches ? '10.8rem' : '7.8rem'}
+                height={matches ? '10.8rem' : '7.8rem'}
+              />
             </div>
             <Group position="apart" align="flex-start" justify="center" w="70%" my="auto">
               <Stack spacing="0.2rem" maw="fit-content" justify="center">
