da060370c35c2e51d622bb81da9c5c2e544e6685
aesumin <2607gk@naver.com>
2023-05-13 01:13:26 +0900

    style: add responsive display

--- client/src/components/OrderComplete/OrderInfo.jsx
+++ client/src/components/OrderComplete/OrderInfo.jsx
@@ -18,38 +18,58 @@ const OrderInfo = ({ history }) => {
       <Title fz="2.4rem" mb="2rem" sx={{ textAlign: 'center' }}>
         주문 정보
       </Title>
-      <Group
-        py="1.6rem"
-        px={matches ? '3.2rem' : '1.6rem'}
-        spacing={matches ? '3.2rem' : '1.6rem'}
-        sx={{ border: '1px solid lightgray', borderRadius: '5px', flexWrap: 'nowrap' }}>
-        <Stack miw="7rem" w="10%">
-          <Text>받는 사람</Text>
-          <Text>주소</Text>
-          <Text>전화번호</Text>
-        </Stack>
-        <Stack miw="18rem" w="70%">
-          <Text>{deliveryAddress.recipient}</Text>
-          <Text>{address}</Text>
-          <Text>{deliveryAddress.recipientPhone}</Text>
-        </Stack>
-      </Group>
-      <Group
-        py="1.6rem"
-        px={matches ? '3.2rem' : '1.6rem'}
-        spacing={matches ? '3.2rem' : '1.6rem'}
-        sx={{ border: '1px solid lightgray', borderRadius: '5px', flexWrap: 'nowrap' }}>
-        <Stack miw="7rem" w="10%">
-          <Text>결제 금액</Text>
-          <Text>결제 방식</Text>
-          <Text>결제 시각</Text>
-        </Stack>
-        <Stack miw="18rem" w="70%">
-          <Text>{discountedTotalPrice.toLocaleString()} 원</Text>
-          <Text>{payment}</Text>
-          <Text>{orderedDate.toLocaleString('ko-KR')}</Text>
-        </Stack>
-      </Group>
+      <Stack py="1.6rem" px={matches ? '3.2rem' : '1.6rem'} sx={{ border: '1px solid lightgray', borderRadius: '5px' }}>
+        <Group spacing={matches ? '3.2rem' : '1.6rem'} sx={{ flexWrap: 'nowrap' }}>
+          <Text miw="7rem" w="10%">
+            수령인
+          </Text>
+          <Text miw="18rem" w="70%">
+            {deliveryAddress.recipient}
+          </Text>
+        </Group>
+        <Group spacing={matches ? '3.2rem' : '1.6rem'} sx={{ flexWrap: 'nowrap' }}>
+          <Text miw="7rem" w="10%">
+            주소
+          </Text>
+          <Text miw="18rem" w="70%">
+            {address}
+          </Text>
+        </Group>
+        <Group spacing={matches ? '3.2rem' : '1.6rem'} sx={{ flexWrap: 'nowrap' }}>
+          <Text miw="7rem" w="10%">
+            전화번호
+          </Text>
+          <Text miw="18rem" w="70%">
+            {deliveryAddress.recipientPhone}
+          </Text>
+        </Group>
+      </Stack>
+      <Stack py="1.6rem" px={matches ? '3.2rem' : '1.6rem'} sx={{ border: '1px solid lightgray', borderRadius: '5px' }}>
+        <Group spacing={matches ? '3.2rem' : '1.6rem'} sx={{ flexWrap: 'nowrap' }}>
+          <Text miw="7rem" w="10%">
+            결제 금액
+          </Text>
+          <Text miw="18rem" w="70%">
+            {discountedTotalPrice.toLocaleString()} 원
+          </Text>
+        </Group>
+        <Group spacing={matches ? '3.2rem' : '1.6rem'} sx={{ flexWrap: 'nowrap' }}>
+          <Text miw="7rem" w="10%">
+            결제 방식
+          </Text>
+          <Text miw="18rem" w="70%">
+            {payment}
+          </Text>
+        </Group>
+        <Group spacing={matches ? '3.2rem' : '1.6rem'} sx={{ flexWrap: 'nowrap' }}>
+          <Text miw="7rem" w="10%">
+            결제 시각
+          </Text>
+          <Text miw="18rem" w="70%">
+            {orderedDate.toLocaleString('ko-KR')}
+          </Text>
+        </Group>
+      </Stack>
     </Stack>
   );
 };
