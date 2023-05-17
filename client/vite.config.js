8ce5c9200e714fb6e604157f615f0cc6a86e98af
LEE DONG GYU <tenenger7125@gmail.com>
2023-05-17 19:30:08 +0900

    feat: add import sort, props sort

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import jsconfigPaths from 'vite-jsconfig-paths';
 
 const PORT = 8000;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), jsconfigPaths()],
   server: {
     proxy: {
       '/api': {
